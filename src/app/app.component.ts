bc0a3339ddad93030bcbddf646dd3e7e749980a5
Julien PRADO <julien.prado@gmail.com>
2019-08-28 21:02:24 +0200

    [evol] securite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,50 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs/Observable';
-import { HexMap } from './hexmap/hexmap';
-import { HexMapService } from './hexmap/hexmap.service';
-import { StageComponent } from './stage/stage.component';
-import { profileIsland, profileDefault } from 'hexmap-lib';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+} from "@angular/core";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Observable } from "rxjs/Observable";
+import { HexMap } from "./hexmap/hexmap";
+import { HexMapService } from "./hexmap/hexmap.service";
+import { StageComponent } from "./stage/stage.component";
+import { profileIsland, profileDefault } from "hexmap-lib";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-
   @ViewChild(StageComponent) stage: StageComponent;
   seed = 0;
   depth = 0;
   width = 30;
   height = 30;
   noiseImpact = 0.1;
-  profiles: { name: string, profile: number[][][][] }[] = [
-    { name: '', profile: null },
+  profiles: { name: string; profile: number[][][][] }[] = [
+    { name: "", profile: null },
     {
-      name: 'profileIsland',
-      profile: profileIsland
+      name: "profileIsland",
+      profile: profileIsland,
     },
     {
-      name: 'profileDefault',
-      profile: profileDefault
+      name: "profileDefault",
+      profile: profileDefault,
     },
-
   ];
-  selectedProfile: { name: string, profile: number[][][][] } = this.profiles[0];
+  selectedProfile: { name: string; profile: number[][][][] } = this.profiles[0];
   private map: HexMap;
 
-  constructor(private _mapService: HexMapService) { }
+  constructor(private _mapService: HexMapService) {}
 
   getMap(): void {
-    this._mapService.getMap()
-      .subscribe(map => {
-        this.map = map;
-        this.stage.updateMapLayer(this.map.hexes);
-      });
+    this._mapService.getMap().subscribe(map => {
+      this.map = map;
+      this.stage.updateMapLayer(this.map.hexes);
+    });
   }
 
   generateMap() {
@@ -51,13 +54,11 @@ export class AppComponent implements OnInit {
       width: this.width,
       height: this.height,
       noiseImpact: this.noiseImpact,
-      profile: this.selectedProfile.profile
+      profile: this.selectedProfile.profile,
     });
   }
 
   public ngOnInit() {
     this.getMap();
   }
-
-
 }
--- src/app/hexmap/hexmap.service.ts
+++ src/app/hexmap/hexmap.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { of } from 'rxjs/observable/of';
-import { HexMap } from './hexmap';
-import { Hexagon } from './hexagon';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { Generator, Options } from 'hexmap-lib';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { of } from "rxjs/observable/of";
+import { HexMap } from "./hexmap";
+import { Hexagon } from "./hexagon";
+import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
+import { Generator, Options } from "hexmap-lib";
 
 @Injectable()
 export class HexMapService {
   private _ObsHexMap = new Subject<HexMap>();
 
-  constructor() { }
+  constructor() {}
 
   getMap(): Observable<HexMap> {
     return this._ObsHexMap.asObservable();
@@ -20,7 +20,7 @@ export class HexMapService {
   generate(opt: Options) {
     const map = new HexMap();
     const generator = new Generator(opt);
-    map.hexes = generator.generate().reduce(function (accu, curr) {
+    map.hexes = generator.generate().reduce(function(accu, curr) {
       accu.push(new Hexagon(curr));
       return accu;
     }, []);
